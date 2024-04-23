ea01283a0ef6e2b3dc0bcbd15b0e094dfbbf42bd
elev <elev@5CD132CY9W.learnet.se>
2024-04-23 10:06:17 +0200

    Added ESLint Configs and Plugins; and updated .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/stylistic',
+		'plugin:github/typescript',
+		'plugin:sonarjs/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:jsx-a11y/recommended',
+		'google',
+		'react-app',
+		'problems',
+		'eslint-config-clean-typescript',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'github', 'sonarjs', 'unicorn', 'jsx-a11y', 'notice'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'notice/notice': [
+			'error',
+			{
+				mustMatch: 'Copyright \\(c\\) [0-9]{0,4}, Nick Deis',
+				template: '/** Copyright (c) <%= YEAR %>, Nick Deis **/',
+			},
+		],
+	},
+};
